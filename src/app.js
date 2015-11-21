01c2e33e3fd5f5f797048510aeada87b91ab0a25
unknown <vendor-klogan@TL-13751-1.daktronics.lan>
2015-11-20 21:18:39 -0600

    login page bootstrapepd form

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 export class App {
-  configureRouter(config, router) {
-    config.title = 'PTO';
+  configureRouter(config, router) { 
+    config.title = 'PTO Tracker';
     config.map([
-    //  { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' }
+      { route: ['', 'public/login'], name: 'login', moduleId: 'public/login', nav: false, title: 'Login' }
     ]);
 
     this.router = router;
