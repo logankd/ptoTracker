21fc5ac8e9007d1f910204baf253a2696f5748a9
unknown <vendor-klogan@TL-13751-1.daktronics.lan>
2015-11-20 21:01:34 -0600

    added bootstrap, fixed style like

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'PTO';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' }
+    //  { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' }
     ]);
 
     this.router = router;
