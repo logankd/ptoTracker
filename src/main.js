0f22c2e0e672f5ce5a1ab360209aae55feb845fc
unknown <vendor-klogan@TL-13751-1.daktronics.lan>
2015-11-23 13:43:30 -0600

    plugin animator move up

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
-
-  //Uncomment the line below to enable animation.
-  aurelia.use.plugin('aurelia-animator-css');
+    .developmentLogging()
+    // enable animation
+    .plugin('aurelia-animator-css');
 
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
